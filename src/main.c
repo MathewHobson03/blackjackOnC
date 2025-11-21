2c737a57d92c8fa6221a846fddf10c321024e845
MathewHobson03 <amandacody06@gmail.com>
2025-11-21 09:13:27 -0700

    Add LCD graphics library and hardware abstraction layer
    - Introduced font handling with font.h and font_table structure.
    - Implemented low-level and high-level drawing functions in lcd_graphic.c.
    - Created hardware abstraction layer (HAL) for memory mapping in hal-api.c.
    - Developed LCD peripheral management in lcd.c, including initialization, cleanup, and drawing functions.
    - Added support for text rendering on the LCD with font integration.
    - Included necessary headers and defined memory addresses for hardware components.
    - Established a clear structure for managing LCD canvas and frame buffer.

--- src/main.c
+++ src/main.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <time.h>
+#include <unistd.h>
+#include "../include/peripherals/lcd.h"
 int main(){
     Card deck[52];
     int playerScoreLocal;
     int dealerScoreLocal;
     initGame(deck, &playerScoreLocal, &dealerScoreLocal);
 
+     lcd_handle_t lcd = {0};
+    printf("starting lcd activity");
+
+    if(lcd_init(&lcd) !=0){
+        printf("Failed to init LCD");
+        return -1;
+
+    }
     while (true) {
         int hit = 0;
         int hit2 = 0;
@@ -28,12 +38,18 @@ int main(){
             while (true) {
                 hit2 = 0;
                 playerHit(deck);
+                printf("...");
+                sleep(1);
+               
                 printPlayerCard();
 
                 if (getPlayerScore() < 21) {
-                    printf("Would you like to hit again? 1 for yes or 2 for no: ");
+                    lcd_write_text(&lcd, 0,0, "Would you like to");
+                    lcd_write_text(&lcd,0,10,"Hit, 1 for yes 2 for no");
+                    
                     fflush(stdout);
                     if (scanf("%d", &hit2) != 1) {
+                        lcd_clear(&lcd);
                         printf("Invalid input. Stopping hits.\n");
                         break;
                     }
@@ -48,6 +64,7 @@ int main(){
         // Run dealer turn and evaluate outcome
         printf("Final player score: %d\n", getPlayerScore());
         printf("Dealer plays...\n");
+        sleep(1);
         dealerPlay(deck);
         printf("Dealer's cards:\n");
         printDealerCard();
